c85b9bc086525eab818e0ec61c818bfa0d07cc53
joustava <joustava@gmail.com>
2021-01-13 09:56:37 +0100

    testing support with Catch2

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to build test target.
+#
+# Go into the directory where this bash script is contained.
+cd `dirname $0`
+
+#./clean.sh
+
+# Compile code.
+mkdir -p build
+cmake -S ./tests -B build
+cd build
+make $*
+
+./build/particle_filter_tests
---
+++ tests/particle_filter_test.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch.hpp>
+// #include "particle_filter.h"
+
+TEST_CASE("Initialise", "[Particle Filter]") {
+  REQUIRE( 1 != 1);
+}
