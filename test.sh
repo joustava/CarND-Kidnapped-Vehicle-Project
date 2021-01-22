4e9c4a0161f85ba0f3c7ff50493b17252f666879
joustava <joustava@gmail.com>
2021-01-22 17:58:42 +0100

    update readme

--- test.sh
+++ test.sh
@@ -4,12 +4,4 @@
 # Go into the directory where this bash script is contained.
 cd `dirname $0`
 
-#./clean.sh
-
-# Compile code.
-mkdir -p build
-cmake -S ./tests -B build
-cd build
-make $*
-
 ./bin/particle_filter_tests
